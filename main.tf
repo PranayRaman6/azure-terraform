fcc402cc0e2cdaec1527531f5329ca88b6bd683f
Paul Yu <pauldotyu@gmail.com>
2020-10-23 09:35:34 -0700

    three layer cake

--- main.tf
+++ main.tf
@@ -16,18 +16,68 @@ module "enterprise_scale" {
   root_parent_id = var.tenant_id
 
   # Optional Variables
-  root_id      = "py"                  // This string variable is used for the root Management Group ID and as a prefix for all core Management Group IDs
-  root_name    = "Paul's Campus Cloud" // This string variable is used as a friendly name for the root Management Group
-  library_path = "${path.root}/lib"    // This string variable is used to define the location of your custom library
+  root_id                   = "py"                // This string variable is used for the root Management Group ID and as a prefix for all core Management Group IDs
+  root_name                 = "Paul Campus Cloud" // This string variable is used as a friendly name for the root Management Group
+  deploy_core_landing_zones = false               // Control whether to deploy the default core landing zones // default = true
+  deploy_demo_landing_zones = false               // Control whether to deploy the demo landing zones (default = false)
+  library_path              = "${path.root}/lib"  // This string variable is used to define the location of your custom library
 
   custom_landing_zones = {
     py-platform = {
-      display_name               = "Paul's Platform"
+      display_name               = "Paul Platform"
       parent_management_group_id = "py"
       subscription_ids           = []
 
       archetype_config = {
         archetype_id   = "py_platform"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    py-landing-zone = {
+      display_name               = "Paul Landing Zone"
+      parent_management_group_id = "py"
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "py_landing_zone"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    py-sandbox = {
+      display_name               = "Paul Sandbox"
+      parent_management_group_id = "py"
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "py_sandbox"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    py-legacy = {
+      display_name               = "Paul Legacy"
+      parent_management_group_id = "py"
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "py_legacy"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    py-qurantine = {
+      display_name               = "Paul Quarantine"
+      parent_management_group_id = "py"
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "py_quarantine"
         parameters     = {}
         access_control = {}
       }
