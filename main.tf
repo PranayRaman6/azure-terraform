1e379f54e725d733768da02356f38c4ae3430059
Paul Yu <pauldotyu@gmail.com>
2020-10-26 06:26:15 -0700

    es cleanup

--- main.tf
+++ main.tf
@@ -3,11 +3,6 @@ provider "azurerm" {
   features {}
 }
 
-variable "tenant_id" {
-  type        = string
-  description = "The tenant_id is used to set the root_parent_id value for the enterprise_scale module."
-}
-
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
   version = "0.0.6-preview"
@@ -16,71 +11,141 @@ module "enterprise_scale" {
   root_parent_id = var.tenant_id
 
   # Optional Variables
-  root_id                   = "py"                // This string variable is used for the root Management Group ID and as a prefix for all core Management Group IDs
-  root_name                 = "Paul Campus Cloud" // This string variable is used as a friendly name for the root Management Group
-  deploy_core_landing_zones = false               // Control whether to deploy the default core landing zones // default = true
-  deploy_demo_landing_zones = false               // Control whether to deploy the demo landing zones (default = false)
-  library_path              = "${path.root}/lib"  // This string variable is used to define the location of your custom library
+  root_id                   = local.root_id      // This string variable is used for the root Management Group ID and as a prefix for all core Management Group IDs
+  root_name                 = local.root_name    // This string variable is used as a friendly name for the root Management Group
+  deploy_core_landing_zones = true               // Control whether to deploy the default core landing zones // default = true
+  deploy_demo_landing_zones = false              // Control whether to deploy the demo landing zones (default = false)
+  library_path              = "${path.root}/lib" // This string variable is used to define the location of your custom library
 
   custom_landing_zones = {
-    py-platform = {
-      display_name               = "Paul Platform"
-      parent_management_group_id = "py"
+    ycc-platform = {
+      display_name               = "YCC Core"
+      parent_management_group_id = local.root_id
       subscription_ids           = []
 
       archetype_config = {
-        archetype_id   = "py_platform"
+        archetype_id   = "ycc_platform"
         parameters     = {}
         access_control = {}
       }
     }
 
-    py-landing-zone = {
-      display_name               = "Paul Landing Zone"
-      parent_management_group_id = "py"
+    ycc-identity = {
+      display_name               = "YCC Identity"
+      parent_management_group_id = "${local.root_id}-platform"
       subscription_ids           = []
 
       archetype_config = {
-        archetype_id   = "py_landing_zone"
+        archetype_id   = "ycc_platform"
         parameters     = {}
         access_control = {}
       }
     }
 
-    py-sandbox = {
-      display_name               = "Paul Sandbox"
-      parent_management_group_id = "py"
+    ycc-connectivity = {
+      display_name               = "YCC Connectivity"
+      parent_management_group_id = "${local.root_id}-platform"
       subscription_ids           = []
 
       archetype_config = {
-        archetype_id   = "py_sandbox"
+        archetype_id   = "ycc_platform"
         parameters     = {}
         access_control = {}
       }
     }
 
-    py-legacy = {
-      display_name               = "Paul Legacy"
-      parent_management_group_id = "py"
+    ycc-management = {
+      display_name               = "YCC Management"
+      parent_management_group_id = "${local.root_id}-platform"
       subscription_ids           = []
 
       archetype_config = {
-        archetype_id   = "py_legacy"
+        archetype_id   = "ycc_platform"
         parameters     = {}
         access_control = {}
       }
     }
 
-    py-qurantine = {
-      display_name               = "Paul Quarantine"
-      parent_management_group_id = "py"
+    ycc-landing-zones = {
+      display_name               = "YCC Baseline"
+      parent_management_group_id = local.root_id
       subscription_ids           = []
 
       archetype_config = {
-        archetype_id   = "py_quarantine"
+        archetype_id   = "ycc_landing_zone"
         parameters     = {}
         access_control = {}
       }
+    }
+
+    ycc-sandboxes = {
+      display_name               = "YCC Sandbox"
+      parent_management_group_id = local.root_id
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "ycc_sandbox"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    ycc-legacy = {
+      display_name               = "YCC Legacy"
+      parent_management_group_id = local.root_id
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "ycc_legacy"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    ycc-qurantine = {
+      display_name               = "YCC Quarantine"
+      parent_management_group_id = local.root_id
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "ycc_quarantine"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+
+    ycc-decommissioned = {
+      display_name               = "YCC Decommissioned"
+      parent_management_group_id = local.root_id
+      subscription_ids           = []
+
+      archetype_config = {
+        archetype_id   = "ycc-decommissioned"
+        parameters     = {}
+        access_control = {}
+      }
+    }
+  }
+
+  archetype_config_overrides = {
+    root = {
+      archetype_id = "ycc_root"
+      parameters = {
+        ES-Allowed-Locations = {
+          listOfAllowedLocations = [
+            "westus2",
+            "westcentralus"
+          ]
+        }
+
+        ES-Allowed-RSG-Locations = {
+          listOfAllowedLocations = [
+            "westus2",
+            "westcentralus"
+          ]
+        }
+      }
+      access_control = {}
     }
   }
 }
